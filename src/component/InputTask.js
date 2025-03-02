00493590e4dc9744f73c6104c1b54445c0162e6b
Louis Asare <louisasare@hotmail.com>
2025-03-02 15:17:10 +0000

    added Input and task

---
+++ src/component/InputTask.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../css/button.css';
+
+function InputTask(){
+    return(
+        <div>
+            <input></input>
+            <button class="button-49" role="button">add task</button>
+            <button class="button-49" role="button">remove task</button>
+        </div>
+        
+    );
+};
+
+export default InputTask;
+
